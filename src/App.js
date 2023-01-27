dc57145b452d3771dda6060173bafd98e0f72b7f
shelter1120 <shelterk1120>
2023-01-27 13:31:47 +0530

    to overcome props use ContetxtApi

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './components/store/auth-context';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -17,14 +18,20 @@ function App() {
     setIsLoggedIn(false);
   };
 
+
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <AuthContext.Provider
+      value={{
+        isLoggedIn:isLoggedIn,
+        onLogout : logoutHandler
+      }}>
+      <MainHeader  onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
+
   );
 }
 
